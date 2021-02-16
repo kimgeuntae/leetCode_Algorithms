0c430c4f452615c807ae0a3101c9bf493484eedb
geuntae.kim <kok617@naver.com>
2021-02-17 03:26:13 +0900

    Add Error Case [digits7]

--- algorithms_problems/src/problem_66/PlusOne.java
+++ algorithms_problems/src/problem_66/PlusOne.java
@@ -10,7 +10,9 @@ package problem_66;
  * 	in:	[0] 		out: [1]
  * 	in:	[1,9,0,9] 	out: [1,9,1,0]
  * 	in:	[2,9,9,9] 	out: [3,0,0,0]
- * 	in:	[9,8,7,6,5,4,3,2,1,0] 	out: [3,0,0,0]
+ * 	in:	[9,8,7,6,5,4,3,2,1,0] 	out: [9,8,7,6,5,4,3,2,1,1]
+ * 	in:	[7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6]
+ * 	out: [7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,7]
  *
  */
 public class PlusOne {
@@ -22,8 +24,8 @@ public class PlusOne {
 			tempNumStr += num;
 		}
 		
-		int numPlusOne = Integer.parseInt(tempNumStr)+1;
-		String temp = Integer.toString(numPlusOne);
+		long numPlusOne = Long.parseLong(tempNumStr)+1;
+		String temp = Long.toString(numPlusOne);
 		digits = new int [temp.length()];
 		
 		for (int i = 0; i < digits.length; i++) {
@@ -34,7 +36,7 @@ public class PlusOne {
     }
 	
 	public static void main(String[] args) {
-		// TODO Have to Solve case6
+		// TODO Have to Solve case7
 		
 		int [] digits1 = {1,2,3};
 		int [] digits2 = {4,3,2,1};
@@ -42,6 +44,7 @@ public class PlusOne {
 		int [] digits4 = {1,9,0,9};
 		int [] digits5 = {2,9,9,9};
 		int [] digits6 = {9,8,7,6,5,4,3,2,1,0};
+		int [] digits7 = {7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6};
 		
 		System.out.println(plusOne(digits1));
 		System.out.println(plusOne(digits2));
@@ -49,6 +52,7 @@ public class PlusOne {
 		System.out.println(plusOne(digits4));
 		System.out.println(plusOne(digits5));
 		System.out.println(plusOne(digits6));
+		System.out.println(plusOne(digits7));
 		
 	}
 }
