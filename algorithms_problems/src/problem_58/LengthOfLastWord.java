f44481b839db214a2e98b4abdf5ec567af59f0ce
geuntae.kim <kok617@naver.com>
2021-02-18 00:06:04 +0900

    Add Initial Problem_58 [LengthOfLastWord]

---
+++ algorithms_problems/src/problem_58/LengthOfLastWord.java
@@ -0,0 +1,35 @@
+package problem_58;
+
+/*	
+ * 	Name: Kim Geuntae
+ * 	Github: https://github.com/kimgeuntae/leetCode_Algorithms
+ * 	FileName : LengthOfLastWord.java
+ *
+ *	in:	s = "Hello World" 	out: 5
+ * 	in:	s = " " 			out: 0
+ * 	in:	s = " a e dase ee" 	out: 2
+ *
+ */
+
+public class LengthOfLastWord {
+	public static int lengthOfLastWord(String s) {
+		
+		if(s.lastIndexOf(" ") == s.length()) {
+			return 0;
+		} else {
+			s = s.substring(s.lastIndexOf(" ")+1, s.length());
+			return s.length();
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		String str1 = "Hello World";
+		String str2 = " ";
+		String str3 = " a e dase ee";
+		
+		System.out.println(lengthOfLastWord(str1));
+		System.out.println(lengthOfLastWord(str2));
+		System.out.println(lengthOfLastWord(str3));
+	}
+}
