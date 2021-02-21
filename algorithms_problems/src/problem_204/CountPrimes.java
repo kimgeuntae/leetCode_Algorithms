9e340176da796e14c6ec6c3f394a8b893876693e
geuntae.kim <kok617@naver.com>
2021-02-21 21:28:19 +0900

    Add Initial Problem_204 [CountPrimes]

---
+++ algorithms_problems/src/problem_204/CountPrimes.java
@@ -0,0 +1,60 @@
+package problem_204;
+
+/*	
+ * 	Name: Kim Geuntae
+ * 	Github: https://github.com/kimgeuntae/leetCode_Algorithms
+ * 	FileName : CountPrimes.java
+ *
+ * 	in:	10	out: 4	Explanation: 2, 3, 5, 7
+ * 	in:	0	out: 0
+ * 	in:	1	out: 0
+ * 	in:	6	out: 3	Explanation: 2, 3, 5
+ * 	in:	3	out: 1	Explanation: 2
+ *  
+ *  [solved]
+ *  Runtime:
+ *  Memory Usage:
+ *  
+ */
+
+public class CountPrimes {
+	public static int countPrimes(int n) {
+		int primeCount = 0;
+		int nonPrimeCount;
+		
+		if(n > 1) {
+			
+			for(int i=2; i< n; i++) {
+				nonPrimeCount=0;
+				
+				for(int j=2; j<i-1; j++) {
+					if(i%j == 0) {
+						nonPrimeCount++;
+						break;
+					}
+				}
+				
+				if(nonPrimeCount == 0) {
+					primeCount++;
+				}
+			}
+		}
+		
+		return primeCount;
+	}
+	
+	public static void main(String[] args) {
+		int n1 = 10;
+		int n2 = 0;
+		int n3 = 1;
+		int n4 = 6;
+		int n5 = 3;
+		
+		System.out.println(countPrimes(n1));
+		System.out.println(countPrimes(n2));
+		System.out.println(countPrimes(n3));
+		System.out.println(countPrimes(n4));
+		System.out.println(countPrimes(n5));
+		
+	}
+}
