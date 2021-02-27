5b9677fc53a24970679e5fcac5e79f46ccf80705
geuntae.kim <kok617@naver.com>
2021-02-27 22:12:50 +0900

    Add Solved Problem_101 [SymmetricTree]

--- algorithms_problems/src/problem_101/SymmetricTree.java
+++ algorithms_problems/src/problem_101/SymmetricTree.java
@@ -9,19 +9,31 @@ package problem_101;
  * 	in:	root = [1,2,2,null,3,null,3] out: false
  *  
  *  [solved]
- *  Runtime:
- *  Memory Usage:
+ *  Runtime: 0 ms, faster than 100.00%
+ *  Memory Usage: 37.3 MB, less than 40.16%
  *  
  */
 
 public class SymmetricTree {
 	
 	public static boolean isSymmetric(TreeNode root) {
-        
-		return true;
+		return isSymmetric(root.left, root.right);
     }
 	
+	private static boolean isSymmetric(TreeNode l1, TreeNode r1) {
+		
+		if(l1 == null || r1 == null) return (l1 == r1);
+		
+		if(l1.val == r1.val) {
+			if(!isSymmetric(l1.left, r1.right)) return false;
+			if(!isSymmetric(l1.right, r1.left)) return false;
+		} else return false;
+		
+		return true;
+	}
+	
 	private static void inOrder(TreeNode node) {
+		
 		if(node != null) {
 			inOrder(node.left);
 			System.out.print(node.val + " ");
@@ -30,6 +42,7 @@ public class SymmetricTree {
 	}
 	
 	private static void reverseOrder(TreeNode node) {
+		
 		if(node != null) {
 			inOrder(node.right);
 			System.out.print(node.val + " ");
@@ -51,7 +64,21 @@ public class SymmetricTree {
 		TreeNode l2 = new TreeNode(2, l3, l4);
 		TreeNode n1 = new TreeNode(1, l2, r2);
 		
-		inOrder(n1);
+		System.out.println(isSymmetric(n1));
+		
+//				1
+//			2		2
+//		  0   3   0   3
+		
+		TreeNode t7 = new TreeNode(3, null, null);
+		TreeNode t6 = new TreeNode(0, null, null);
+		TreeNode t5 = new TreeNode(2, t6, t7);
+		TreeNode t4 = new TreeNode(3, null, null);
+		TreeNode t3 = new TreeNode(0, null, null);
+		TreeNode t2 = new TreeNode(2, t3, t4);
+		TreeNode t1 = new TreeNode(1, t2, t5);
+		
+		System.out.println(isSymmetric(t1));
 		
 	}
 }
