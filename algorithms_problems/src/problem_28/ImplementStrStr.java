51eb1f2823decb215f729e6419b82bc0a25a222b
geuntae.kim <kok617@naver.com>
2021-02-27 22:57:28 +0900

    Runtime Error [haystack4, needle4]

--- algorithms_problems/src/problem_28/ImplementStrStr.java
+++ algorithms_problems/src/problem_28/ImplementStrStr.java
@@ -18,14 +18,49 @@ package problem_28;
 public class ImplementStrStr {
 	
 	public static int strStr(String haystack, String needle) {
+		// TODO Runtime Error [haystack4, needle4]
+	    
+		int l1 = haystack.length(), l2 = needle.length();
+		
+		if(l1 < l2) {
+			return -1;
+		} else if(l2 == 0) {
+			return 0;
+		} else {
+			String needleFitstWord = needle.substring(0, 1);
+			
+			for(int i=0; i<l1;) {
+				if(l1 < i+l2) return -1;
+				String tempStr = haystack.substring(i, i+l2);
+				
+				if(tempStr.equals(needle)) {
+					return i;
+				} else if(tempStr.contains(needleFitstWord)) {
+					i += tempStr.indexOf(needleFitstWord);
+					tempStr = haystack.substring(i, i+l2);
+					if(tempStr.equals(needle)) return i;
+					
+					i++;
+				} else {
+					i += l2;
+				}
+			}
+		}
 		
 		return -1;
 	}
 	
 	public static void main(String[] args) {
 		
-		String haystack = "hello", needle = "ll";
+		String haystack1 = "hello", needle1 = "ll";
+		String haystack2 = "aaaaa", needle2 = "bba";
+		String haystack3 = "", needle3 = "";
+		String haystack4 = "mississippi", needle4 = "issipi";
 		
-		strStr(haystack, needle);
+		System.out.println(strStr(haystack1, needle1));
+		System.out.println(strStr(haystack2, needle2));
+		System.out.println(strStr(haystack3, needle3));
+		System.out.println(strStr(haystack4, needle4));
+		
 	}
 }
