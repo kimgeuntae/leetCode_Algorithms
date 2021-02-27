569ad81073b512ef3fd0494d87df16ce99cbbf02
geuntae.kim <kok617@naver.com>
2021-02-27 22:46:00 +0900

    Add Initial Problem_28 [ImplementStrStr]

---
+++ algorithms_problems/src/problem_28/ImplementStrStr.java
@@ -0,0 +1,31 @@
+package problem_28;
+
+/*	
+ * 	Name: Kim Geuntae
+ * 	Github: https://github.com/kimgeuntae/leetCode_Algorithms
+ * 	FileName : ImplementStrStr.java
+ *
+ * 	in:	haystack = "hello", needle = "ll"	out: 2
+ * 	in:	haystack = "aaaaa", needle = "bba"	out: -1
+ * 	in:	haystack = "", needle = ""			out: 0
+ *  
+ *  [solved]
+ *  Runtime:
+ *  Memory Usage:
+ *  
+ */
+
+public class ImplementStrStr {
+	
+	public static int strStr(String haystack, String needle) {
+		
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		
+		String haystack = "hello", needle = "ll";
+		
+		strStr(haystack, needle);
+	}
+}
