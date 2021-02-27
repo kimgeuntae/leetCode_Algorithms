c0d30002a7a66d440b3049cf9295933bc7d5175d
geuntae.kim <kok617@naver.com>
2021-02-27 21:00:01 +0900

    Add Solved Problem_02 [AddTwoNumbers]

--- algorithms_problems/src/problem_02/AddTwoNumbers.java
+++ algorithms_problems/src/problem_02/AddTwoNumbers.java
@@ -9,27 +9,43 @@ package problem_02;
  * 	in:	l1 = [0], l2 = [0] 						out: [0]
  *  in:	l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9] 	out: [8,9,9,9,0,0,0,1]
  *
+ *	[solved]
+ *  Runtime: 2 ms, faster than 77.52%
+ *  Memory Usage: 39 MB, less than 94.91%
  */
 
 public class AddTwoNumbers {
 
-	// TODO Change array to listNode
-	
-	private static int listToInt(int [] tempList) {
-		int num = 0;
+	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 		
-		for(int i=tempList.length; i>0; i--) {
-			num += tempList[i-1] * Math.pow(10,  tempList.length-i);
+		ListNode result = new ListNode();
+		ListNode temp = result;
+		int carry = 0;
+		
+		while(l1 != null || l2 != null || carry == 1) {
+			
+			int sum = ((l1 == null) ? 0 : l1.val) + ((l2 == null) ? 0 : l2.val) + carry;
+			ListNode curNode = new ListNode(sum%10);
+			carry = sum / 10;
+			
+			temp.next = curNode;
+			temp = curNode;
+			
+			if(l1 != null) l1 = l1.next;
+			if(l2 != null) l2 = l2.next;
+			
 		}
 		
-		return (int)num;
-	}
+        return result.next;
+    }
 	
 	private static void printListNode(ListNode head) {
+		
 		while(head != null) {
 			System.out.print(head.val + " ");
 			head = head.next;
 		}
+		
 		System.out.println();
 	}
 	
@@ -57,11 +73,13 @@ public class AddTwoNumbers {
 		ListNode listNodeNums2_1 = genListNode(nums2_1);
 		ListNode listNodeNums2_2 = genListNode(nums2_2);
 		
-		printListNode(listNodeNums1_1);
-		printListNode(listNodeNums1_2);
-		printListNode(listNodeNums2_1);
-		printListNode(listNodeNums2_2);
+//		printListNode(listNodeNums1_1);
+//		printListNode(listNodeNums1_2);
+//		printListNode(listNodeNums2_1);
+//		printListNode(listNodeNums2_2);
 		
+		printListNode(addTwoNumbers(listNodeNums1_1, listNodeNums1_2));
+		printListNode(addTwoNumbers(listNodeNums2_1, listNodeNums2_2));
 		
 	}
 }
