238b4581882616a638d96e68b555ce565e39f2fc
geuntae.kim <kok617@naver.com>
2021-03-04 00:05:34 +0900

    Add Initial Problem_67 [AddBinary]

---
+++ algorithms_problems/src/problem_67/AddBinary.java
@@ -0,0 +1,32 @@
+package problem_67;
+
+/*	
+ * 	Name: Kim Geuntae
+ * 	Github: https://github.com/kimgeuntae/leetCode_Algorithms
+ * 	FileName : AddBinary.java
+ *
+ * 	in:	a = "11", b = "1"		out: "100"
+ * 	in:	a = "1010", b = "1011"	out: "10101"
+ *  
+ *  [solved]
+ *  Runtime:
+ *  Memory Usage:
+ *  
+ */
+
+public class AddBinary {
+	public static String addBinary(String a, String b) {
+        
+		return "";
+    }
+	
+	public static void main(String[] args) {
+		
+		String a1 = "11", b1 = "1";			// 100
+		String a2 = "1010", b2 = "1011";	// 10101
+		
+		System.out.println(addBinary(a1, b1));
+		System.out.println(addBinary(a2, b2));
+		
+	}
+}
