e3bc1e027e170a07dee2baba906b4f6519064650
geuntae.kim <kok617@naver.com>
2021-03-04 00:14:35 +0900

    Solved Problem_67 [AddBinary]

--- algorithms_problems/src/problem_67/AddBinary.java
+++ algorithms_problems/src/problem_67/AddBinary.java
@@ -9,15 +9,50 @@ package problem_67;
  * 	in:	a = "1010", b = "1011"	out: "10101"
  *  
  *  [solved]
- *  Runtime:
- *  Memory Usage:
+ *  Runtime: 8 ms, faster than 8.01%
+ *  Memory Usage: 39.1 MB, less than 39.50%
  *  
  */
 
 public class AddBinary {
 	public static String addBinary(String a, String b) {
         
-		return "";
+		char [] binaryAChr = a.toCharArray();
+		char [] binaryBChr = b.toCharArray();
+		
+		int aIndex = a.length()-1;
+		int bIndex = b.length()-1;
+		String resultBinary = "";
+		int sum = 0;
+		char carry = '0';
+		
+		while(aIndex >= 0 || bIndex >= 0 || carry != '0') {
+			if(aIndex < 0 && bIndex < 0) {
+				sum = carry - '0';
+			} else if(aIndex < 0) {
+				sum = (binaryBChr[bIndex] - '0') + (carry - '0');
+			} else if(bIndex < 0) {
+				sum = (binaryAChr[aIndex] - '0') + (carry - '0');
+			} else {
+				sum = (binaryAChr[aIndex] - '0') + (binaryBChr[bIndex] - '0') + (carry - '0');
+			}
+			
+			resultBinary += sum % 2;
+			carry = Character.forDigit(sum/2, 10);
+			
+			aIndex--;
+			bIndex--;
+		}
+		
+		String temp = "";
+		
+		for(int i=resultBinary.length()-1; i>=0; i--) {
+			temp += resultBinary.charAt(i);
+		}
+		
+		resultBinary = temp;
+		
+		return resultBinary;
     }
 	
 	public static void main(String[] args) {
