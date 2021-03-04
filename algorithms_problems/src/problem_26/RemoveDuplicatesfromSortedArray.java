802f1e995867f088d83bedacb779a6adb5c09a69
geuntae.kim <kok617@naver.com>
2021-03-05 00:11:33 +0900

    Solved Problem_26 [RemoveDuplicatesfromSortedArray]

--- algorithms_problems/src/problem_26/RemoveDuplicatesfromSortedArray.java
+++ algorithms_problems/src/problem_26/RemoveDuplicatesfromSortedArray.java
@@ -11,15 +11,23 @@ import java.util.Arrays;
  * 	in:	nums = [0,0,1,1,1,2,2,3,3,4]	out: 5, nums = [0,1,2,3,4]
  *  
  *  [solved]
- *  Runtime:
- *  Memory Usage:
+ *  Runtime: 1 ms, faster than 31.02%
+ *  Memory Usage: 40.5 MB, less than 94.26%
  *  
  */
 
 public class RemoveDuplicatesfromSortedArray {
 	public static int removeDuplicates(int[] nums) {
         
-		return nums.length;
+		int size=0;
+		
+		for(int n : nums) {
+			if(nums[size] < n) {
+				nums[++size] = n;
+			}
+		}
+		
+		return size+1;
     }
 	
 	public static void main(String[] args) {
@@ -29,5 +37,6 @@ public class RemoveDuplicatesfromSortedArray {
 		
 		System.out.println(removeDuplicates(nums1) + " " + Arrays.toString(nums1));
 		System.out.println(removeDuplicates(nums2) + " " + Arrays.toString(nums2));
+		
 	}
 }
