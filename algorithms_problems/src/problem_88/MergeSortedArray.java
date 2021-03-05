48518a6d992b1c9161387f3657dbd74a69fee04b
geuntae.kim <kok617@naver.com>
2021-03-05 22:41:33 +0900

    Add test_case [nums3_1]

--- algorithms_problems/src/problem_88/MergeSortedArray.java
+++ algorithms_problems/src/problem_88/MergeSortedArray.java
@@ -20,7 +20,26 @@ import java.util.Arrays;
 
 public class MergeSortedArray {
 	public static void merge(int[] nums1, int m, int[] nums2, int n) {
+		// TODO Error Wrong Answer test_case3
         
+		int nums1Index = m-1, nums2Index = n-1, resultIndex = m+n;
+		int tempNum = 0;
+		
+		while(0 <= nums1Index || 0 <= nums2Index || 0 < resultIndex) {
+			if(nums2Index < 0) {
+				tempNum = nums1[nums1Index--];
+			} else if(nums1Index < 0) {
+				tempNum = nums1[nums2Index--];
+			} else {
+				if(nums2[nums2Index] < nums1[nums1Index]) {
+					tempNum = nums1[nums1Index--];
+				} else {
+					tempNum = nums2[nums2Index--];
+				}
+			}
+			
+			nums1[--resultIndex] = tempNum;
+		}
     }
 	
 	public static void main(String[] args) {
@@ -31,11 +50,17 @@ public class MergeSortedArray {
 		int [] nums2_1 = {1}, nums2_2 = {};
 		int m2 = 1, n2 = 0;
 		
+		int [] nums3_1 = {0}, nums3_2 = {1};
+		int m3 = 0, n3 = 1;
+		
 		merge(nums1_1, m1, nums1_2, n1);
 		System.out.println(Arrays.toString(nums1_1));
 
 		merge(nums2_1, m2, nums2_2, n2);
 		System.out.println(Arrays.toString(nums2_1));
+		
+		merge(nums3_1, m3, nums3_2, n3);
+		System.out.println(Arrays.toString(nums3_1));
 		
 	}
 }
