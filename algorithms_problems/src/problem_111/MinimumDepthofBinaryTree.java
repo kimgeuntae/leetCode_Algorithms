d39ba4d46c7737b708ec24506c53e523206b1b98
geuntae.kim <kok617@naver.com>
2021-03-30 00:13:24 +0900

    Add error_case Problem_111 [MinimumDepthofBinaryTree]

--- algorithms_problems/src/problem_111/MinimumDepthofBinaryTree.java
+++ algorithms_problems/src/problem_111/MinimumDepthofBinaryTree.java
@@ -7,6 +7,9 @@ package problem_111;
  *
  * 	in:	root = [3,9,20,null,null,15,7]
  * 	out: 2
+ * 	
+ * 	in:	root = [2,null,3,null,4,null,5,null,6]
+ * 	out: 5
  *  
  *  [solved]
  *  Runtime:
@@ -16,18 +19,24 @@ package problem_111;
 
 public class MinimumDepthofBinaryTree {
 	public static int minDepth(TreeNode root) {
+		//TODO Change Algorithm
+		
         int dep, depL, depR;
         
-        if(root.left == null && root.right == null) {
-        	dep = 1;
+        if(root == null) {
+        	return 0;
         } else {
-        	depL = minDepth(root.left);
-        	depR = minDepth(root.right);
-        	if(depL >= depR) dep = depR+1;
-        	else dep = depL+1;
-        }
+		    if(root.left == null && root.right == null) {
+		    	dep = 1;
+		    } else {
+		    	depL = minDepth(root.left);
+		    	depR = minDepth(root.right);
+		    	if(depL >= depR) dep = depR+1;
+		    	else dep = depL+1;
+		    }
         
-        return dep;
+        	return dep;
+        }
     }
 	
 	public static void main(String[] args) {
@@ -43,6 +52,20 @@ public class MinimumDepthofBinaryTree {
 		TreeNode n1 = new TreeNode(3, n2, n3);
 		
 		System.out.println(minDepth(n1));
+		
+		//		2
+		//			3
+		//				4
+		//					5
+		//						6
+		
+		TreeNode a5 = new TreeNode(6, null, null);
+		TreeNode a4 = new TreeNode(5, null, a5);
+		TreeNode a3 = new TreeNode(4, null, a4);
+		TreeNode a2 = new TreeNode(3, null, a3);
+		TreeNode a1 = new TreeNode(2, null, a2);
+		
+		System.out.println(minDepth(a1));	// 5
 		
 	}
 }
