b7e52ae579ab719668533d12c84aa74219d0cd0b
geuntae.kim <kok617@naver.com>
2021-03-30 23:33:54 +0900

    Add Initial Problem_389 [FindtheDifference]

---
+++ algorithms_problems/src/problem_389/FindtheDifference.java
@@ -0,0 +1,45 @@
+package problem_389;
+
+/*	
+ * 	Name: Kim Geuntae
+ * 	Github: https://github.com/kimgeuntae/leetCode_Algorithms
+ * 	FileName : FindtheDifference.java
+ *
+ * 	in:	abcd , abcde
+ *  out: e
+ * 	in:	a , aa
+ *  out: a
+ *  
+ *  [solved]
+ *  Runtime:
+ *  Memory Usage:
+ *  
+ */
+
+public class FindtheDifference {
+	
+	public static char findTheDifference(String s, String t) {
+		char [] sTemp = s.toCharArray(), tTemp = t.toCharArray();
+		
+		for(int i=0; i<sTemp.length; i++) {
+			if(sTemp[i] != tTemp[i]) {
+				return tTemp[i];
+			}
+		}
+		
+		return tTemp[tTemp.length-1];
+	}
+	
+	public static void main(String[] args) {
+		String s1 = "abcd", t1 = "abcde";
+		String s2 = "", t2 = "y";
+		String s3 = "a", t3 = "aa";
+		String s4 = "ae", t4 = "aea";
+		
+		System.out.println(findTheDifference(s1, t1));
+		System.out.println(findTheDifference(s2, t2));
+		System.out.println(findTheDifference(s3, t3));
+		System.out.println(findTheDifference(s4, t4));
+		
+	}
+}
