2e59e55f60c0e1941651c45ff181b424f3b9bf76
geuntae.kim <kok617@naver.com>
2021-04-18 22:57:41 +0900

    Add Initial Problem_199 [BinaryTreeRightSideView]

---
+++ algorithms_problems/src/problem_199/BinaryTreeRightSideView.java
@@ -0,0 +1,47 @@
+package problem_199;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*	
+ * 	Name: Kim Geuntae
+ * 	Github: https://github.com/kimgeuntae/leetCode_Algorithms
+ * 	FileName : BinaryTreeRightSideView.java
+ *
+ * 	in:	[1,2,3,null,5,null,4] out: [1,3,4]
+ *  
+ *  [solved]
+ *  Runtime:
+ *  Memory Usage:
+ *  
+ */
+
+public class BinaryTreeRightSideView {
+	public static List<Integer> rightSideView(TreeNode root) {
+		
+		List<Integer> temp = new ArrayList<Integer>();
+		
+		if(root != null) {
+			temp.add(root.val);
+			temp.addAll(rightSideView(root.right));
+		}
+		
+		return temp;
+	}
+	
+	public static void main(String[] args) {
+		
+//				1
+//			2		3
+//			   5	   4
+		
+		TreeNode n5 = new TreeNode(5, null, null);
+		TreeNode n4 = new TreeNode(4, null, null);
+		TreeNode n3 = new TreeNode(3, null, n4);
+		TreeNode n2 = new TreeNode(2, null, n5);
+		TreeNode n1 = new TreeNode(1, n2, n3);
+		
+		System.out.println(rightSideView(n1));
+		
+	}
+}
